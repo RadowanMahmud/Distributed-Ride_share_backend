cc455f12dcfaac46887ed1ae519f152e9df76eaf
RadowanMahmud <bsse1002@iit.du.ac.bd>
2021-04-14 11:47:18 +0600

    ride share and communication module seperated

---
+++ communicationModule/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const server = require('http').createServer()
+const sch = require('node-schedule')
+
+const io = require('socket.io')(server)
+
+app.use(cors())
+app.use(express.json())
+
+let storeSocket;
+
+io.of('communication').on('connection', (socket)=>{
+    console.log("new user connected")
+    storeSocket = socket
+})
+
+app.post('/api/communication', (req,res)=>{
+    storeSocket.emit("welcome", req.body.driver_name +' has been matched with '+ req.body.rider_name + ' and the fiar is ' + req.body.mindist*2 +' #'+ req.body.driver_id)
+})
+
+server.listen(8001,()=>{
+    console.log('socket for communication module is opened at port 8001')
+})
+app.listen(8000, () => {
+    console.log('server for communication module is opened at port 8000')
+})
+
